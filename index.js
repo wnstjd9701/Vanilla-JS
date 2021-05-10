bd75f0717f8dbea2b34244254d83676684086ce8
wnstjd9701 <wnstjd9701@naver.com>
2021-05-11 04:55:01 +0900

    Vanilla JS

--- index.js
+++ index.js
@@ -1,72 +1,13 @@
-const par = "I am Working. I am JS. Beautiful, I am worth it";
-console.log(par);
-let a = 221;
-let b = a - 5;
-const c = 100; // constant 변하지 않는 값
-console.log(b, a);
+const title = document.querySelector("#title");
 
+const CLICKED_CLASS = "clicked";
 
-
-// String
-const myname = "Jun";
-console.log(myname);
-
-// Array
-const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-console.log(daysOfWeek[0]);
-
-// Object 
-const junInfo = {
-    name: "Jun",
-    age: 20,
-    gender: "Male",
-    isHandsome: true,
-    favMovies: ["Along the gods", "LOTR", "Oldboy"],
-    favFood: [
-        {
-            name: "Meat",
-            fatty: false
-        },
-        {
-            name: "Cheese burger",
-            fatty: true
-        }
-    ]
+function handleClick(){
+   title.classList.toggle(CLICKED_CLASS); // class check and if it exists -> add / if does not exist -> remove
 }
 
-console.log(junInfo);
-console.log(junInfo.name);
-console.log(junInfo.age);
 
-junInfo.age = 25;
-console.log(junInfo.age);
-
-// Function - function name(){code}
-function sayHello(name, age){
-    console.log("Hello!", name);
-    console.log(`Hello ${name} you are ${age} years`);
+function init(){
+    title.addEventListener("click", handleClick);
 }
-sayHello("Jun", 20);
-
-function Hello(name, age){
-    return `Hello ${name} you are ${age} years`;
-}
-const greetJun = Hello("Jun", 25);
-console.log(greetJun);
-
-const calculator = {
-    plus: function(a,b){
-        return a+b;
-    },
-    multiply: function(a,b){
-        return a*b;
-    },
-    sub: function(a,b){
-        return a-b;
-    },
-    squared: function(a,b){
-        return a**b;
-    }
-}
-const plus = calculator.plus(5, 6);
-console.log(plus);
+init();
