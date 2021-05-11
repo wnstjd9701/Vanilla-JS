18afc3b937db48ba19317f895b77a66ef635e1de
wnstjd9701 <wnstjd9701@naver.com>
2021-05-12 06:07:27 +0900

    Vanilla Js

--- greeting.js
+++ greeting.js
@@ -1,10 +1,26 @@
 // local storage
-const form = document.querySelector(".js-form");
-input = form.querySelector("input"),
-greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser";
     SHOWING_ON = "showing";
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    painGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_ON);
+    form.addEventListener("submit",handleSubmit)
+}
 
 function painGreeting(text){
         form.classList.remove(SHOWING_ON);
@@ -15,6 +31,7 @@ function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser == null){
         // she is not
+        askForName();
     } else {
         // she if
         painGreeting(currentUser);
