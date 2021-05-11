cdc061d493c5e7cc2df7626858ac16c61e001c27
wnstjd9701 <wnstjd9701@naver.com>
2021-05-12 04:19:40 +0900

    Vanilla Js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-   title.classList.toggle(CLICKED_CLASS); // class check and if it exists -> add / if does not exist -> remove
+   title.classList.toggle(CLICKED_CLASS); // class check and if it exists -> classList.add / if does not exist -> classList.remove
 }
 
 function init(){
     title.addEventListener("click", handleClick);
 }
+
+// init 실행
 init();
