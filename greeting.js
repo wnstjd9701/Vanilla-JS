b45927068075c9fcd5bcbd81c82f74b9bc991488
wnstjd9701 <wnstjd9701@naver.com>
2021-05-13 02:01:53 +0900

    Vanilla Js

--- greeting.js
+++ greeting.js
@@ -1,43 +1,43 @@
-// local storage
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser";
-    SHOWING_ON = "showing";
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
 
-function saveName(text){
-    localStorage.setItem(USER_LS, text);
-}
-
-function handleSubmit(event){
-    event.preventDefault();
-    const currentValue = input.value;
-    painGreeting(currentValue);
-    saveName(currentValue);
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
 }
 
-function askForName(){
-    form.classList.add(SHOWING_ON);
-    form.addEventListener("submit",handleSubmit)
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
 }
 
-function painGreeting(text){
-        form.classList.remove(SHOWING_ON);
-    greeting.classList.add(SHOWING_ON);
-    greeting.innerText = `Hello ${text}`;
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
 }
-function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser == null){
-        // she is not
-        askForName();
-    } else {
-        // she if
-        painGreeting(currentUser);
-    }
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
 }
-function init(){
-    loadName();
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
 }
+
+function init() {
+  loadName();
+}
+
 init();
